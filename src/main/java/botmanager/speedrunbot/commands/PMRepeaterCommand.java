8faf3c33648cf95e5ceb80d6a790cd9ac9447b73
= <=>
2020-07-12 10:36:09 -0400

    No more DM spam

--- src/main/java/botmanager/speedrunbot/commands/PMRepeaterCommand.java
+++ src/main/java/botmanager/speedrunbot/commands/PMRepeaterCommand.java
@@ -5,6 +5,7 @@ import botmanager.Utilities;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import botmanager.speedrunbot.generic.SpeedrunBotCommandBase;
+import java.util.ArrayList;
 import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 
 /**
@@ -14,6 +15,8 @@ import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 
 public class PMRepeaterCommand extends SpeedrunBotCommandBase {
 
+    ArrayList<String> messagedUserIDs = new ArrayList();
+    
     public PMRepeaterCommand(BotBase bot) {
         super(bot);
     }
@@ -36,10 +39,11 @@ public class PMRepeaterCommand extends SpeedrunBotCommandBase {
         message = "Sent by " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + "\n" + event.getMessage().getContentRaw();
         Utilities.sendPrivateMessage(bot.getJDA().getUserById("106949500500738048"), message);
         
-        if (event.getMessage().getContentRaw().startsWith(bot.getPrefix())) {
+        if (event.getMessage().getContentRaw().startsWith(bot.getPrefix()) && messagedUserIDs.contains(event.getAuthor().getId())) {
             Utilities.sendPrivateMessage(event.getAuthor(), "I prefer my commands be used on servers rather than DMs. "
                     + "If you have any questions or concerns about this bot though, feel free to voice your opinion through this chat, "
                     + "and they will reach to the developer.");
+            messagedUserIDs.add(event.getAuthor().getId());
         }
     }
 
