ad151af0b3ca030ccfd5b28dd85d8ccd91f529f8
MC2018 <mc2018.git@gmail.com>
2022-05-21 14:30:08 -0400

    Reorganized MassPoll class a bit

--- src/main/java/botmanager/bots/masspoll/MassPoll.java
+++ src/main/java/botmanager/bots/masspoll/MassPoll.java
@@ -9,6 +9,8 @@ import botmanager.bots.masspoll.commands.pollmanagement.ResendCommand;
 import botmanager.bots.masspoll.objects.Poll;
 import botmanager.generic.BotBase;
 import botmanager.generic.ICommand;
+import botmanager.utils.IOUtils;
+import botmanager.utils.JDAUtils;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
@@ -18,41 +20,52 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class MassPoll extends BotBase {
 
-    public HashMap<String, Poll> pollsBeingCreated = new HashMap<>();
-    public ArrayList<Long> pollsInProcess = new ArrayList<>();
+    final public HashMap<String, Poll> pollsBeingCreated = new HashMap<>();
+    final public ArrayList<Long> pollsInProcess = new ArrayList<>();
 
     public MassPoll(String botToken, String name) {
         super(botToken, name);
         getJDA().getPresence().setActivity(Activity.playing("send 'masspoll' in DM"));
 
         setCommands(new ICommand[] {
-                new GuildJoinCommand(this),
+                // Creation
+                new AddMemberCommand(this),
+                new AddOptionCommand(this),
+                new CancelCommand(this),
+                new EditOptionCommand(this),
                 new MassPollCommand(this),
                 new QuestionCommand(this),
-                new AddOptionCommand(this),
-                new RemoveOptionCommand(this),
-                new CancelCommand(this),
-                new SendCommand(this),
-                new VoteCommand(this),
-                new CommentCommand(this),
-                new ResendCommand(this),
-                new PollDataCommand(this),
-                new SelectRolesCommand(this),
-                new AddMemberCommand(this),
                 new RemoveMemberCommand(this),
-                new EditOptionCommand(this),
-                new ReorderOptionCommand(this)
+                new RemoveOptionCommand(this),
+                new ReorderOptionCommand(this),
+                new SelectRolesCommand(this),
+                new SendCommand(this),
+
+                // Polling
+                new CommentCommand(this),
+                new VoteCommand(this),
+
+                // Poll Management
+                new PollDataCommand(this),
+                new ResendCommand(this),
+
+                // Other
+                new GuildJoinCommand(this),
         });
     }
 
     @Override
     public void onButtonClick(ButtonClickEvent event) {
+        event.deferEdit().queue();
+
         for (ICommand command : getCommands()) {
             command.run(event);
         }
